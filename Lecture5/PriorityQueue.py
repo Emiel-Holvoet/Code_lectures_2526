59096a039d4c8417ea7033b5000d1c0ec6ac4650
Emiel-Holvoet <holvoet.emiel@outlook.com>
2025-11-16 11:03:58 +0100

    Translate comments in PriorityQueue.py to Dutch
    Translated comments to Dutch for better understanding.

--- Lecture5/PriorityQueue.py
+++ Lecture5/PriorityQueue.py
@@ -2,19 +2,20 @@ from Heap import Heap
 
 class PriorityQueue:
     def __init__(self):
-        self.__heap = Heap()
+        self.__heap = Heap()           # Interne heap die de prioriteiten beheert
 
-    # Adds an element to this queue
+    # Voeg een element toe aan de priority queue
     def enqueue(self, e):
-        self.__heap.add(e)
-    
-    # Removes an element from this queue
+        self.__heap.add(e)             # In een max-heap komt het grootste element bovenaan
+
+    # Verwijder het element met de hoogste prioriteit
     def dequeue(self):
-        if self.getSize() == 0:
+        if self.getSize() == 0:        # Queue leeg?
             return None
         else:
-            return self.__heap.remove()
-    
-    # Return the size of the queue
+            return self.__heap.remove()  # remove() haalt de root van de heap weg (max element)
+
+    # Geef de grootte van de queue terug
     def getSize(self):
-        return self.__heap.getSize()
+        return self.__heap.getSize()    # Aantal elementen in de heap
+
